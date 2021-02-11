ab9dd32c767a26df76a2fa5d7f576140280ec853
Jakob Persons <jakobep24@gmail.com>
2021-02-11 01:33:02 -0500

    tests passing

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,7 +2,27 @@ import React from "react";
 import { movies } from "../data";
 
 function Movies() {
-  return <div>{/*{code here}*/}</div>;
+
+  const movieComponents = movies.map(movie => {
+    return (
+      <div key={movie.title}>
+        <h2>{movie.title}</h2>
+        <p>Runtime: {movie.time} min.</p>
+        <ul>
+          {movie.genres.map(genre => {
+            return <li key={genre}>{genre}</li>
+          })}
+        </ul>
+      </div>
+    )
+  })
+
+  return (
+    <div>
+      <h1>Movies Page</h1>
+      {movieComponents}
+    </div>
+  );
 }
 
 export default Movies;
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,8 +1,60 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 
+const linkStyles = {
+  width: "100px",
+   padding: "12px",
+   margin: "0 6px 6px",
+   background: "red",
+   textDecoration: "none",
+   color: "white",
+}
+
 function NavBar() {
-  return <div>{/*{code here}*/}</div>;
+  return (
+    <div className="navbar">
+      <NavLink
+        to="/"
+        exact
+        style={linkStyles}
+        activeStyle={{
+          background: "black"
+        }}
+      >
+        Home
+      </NavLink>
+      <NavLink
+        to="/movies"
+        exact
+        style={linkStyles}
+        activeStyle={{
+          background: "black"
+        }}
+      >
+        Movies
+      </NavLink>
+      <NavLink
+        to="/directors"
+        exact
+        style={linkStyles}
+        activeStyle={{
+          background: "black"
+        }}
+      >
+        Directors
+      </NavLink>
+      <NavLink
+        to="/actors"
+        exact
+        style={linkStyles}
+        activeStyle={{
+          background: "black"
+        }}
+      >
+        Actors
+      </NavLink>
+    </div>
+  );
 }
 
 export default NavBar;
